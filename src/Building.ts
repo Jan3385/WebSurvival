6b6a1fa16c5022b807ff11a55a8729fb82ea998b
Hat Dealer <jan.richter2007@gmail.com>
2024-07-15 09:36:47 +0200

    Better labels and Input bar

--- src/Building.ts
+++ src/Building.ts
@@ -10,57 +10,57 @@ let Building = [
     {   //cheap wall
         build: new BuildingData("Cheap Wall", new rgb(244, 211, 94), 1, 1, PixelStatus.block, 3, _Highlight.border, InteractType.wall),
         cost: {stone: 0, wood: 3},
-        label: "Cheap Wall - cheap but weak"
+        label: "Cheap but weak"
     },
     {   //wooden wall
         build: new BuildingData("Wooden Wall", new rgb(127, 79, 36), 1, 1, PixelStatus.block, 12, _Highlight.border, InteractType.wall),
         cost: {stone: 0, wood: 10},
-        label: "Wooden Wall - stronger but more expensive"
+        label: "Stronger but more expensive"
     },
     {   //stone wall
         build: new BuildingData("Stone Wall", new rgb(85, 85, 85), 1, 1, PixelStatus.block, 24, _Highlight.border, InteractType.wall),
         cost: {stone: 15, wood: 2},
-        label: "Stone Wall - strong but expensive"
+        label: "Strong but expensive"
     },
     {   //cheap floor
         build: new BuildingData("Cheap Floor", new rgb(255, 243, 176), 1, 1, PixelStatus.taken, 1, _Highlight.none, InteractType.floor),
         cost: {stone: 0, wood: 1},
-        label: "Cheap Floor - not the prettiest"
+        label: "Not the prettiest"
     },
     {   //wooden floor
         build: new BuildingData("Wooden Floor", new rgb(175, 164, 126), 1, 1, PixelStatus.taken, 3, _Highlight.none, InteractType.floor),
         cost: {stone: 0, wood: 2},
-        label: "Wooden Floor - decent looking"
+        label: "Decent looking"
     },
     {   //stone floor
         build: new BuildingData("Stone Floor", new rgb(206, 212, 218), 1, 1, PixelStatus.taken, 6, _Highlight.none, InteractType.floor),
         cost: {stone: 2, wood: 0},
-        label: "Stone Floor - build with unforseen quality"
+        label: "Build with unforseen quality"
     },
     {   //cheap door
         build: new DoorData("Cheap Door", new rgb(255, 231, 230), 1, 1, PixelStatus.block, 3, _Highlight.slash, InteractType.door),
         cost: {stone: 0, wood: 10},
-        label: "Cheap Door - gets you thru the night"
+        label: "Gets you thru the night"
     },
     {   //wooden door
         build: new DoorData("Wooden Door", new rgb(200, 180, 166), 1, 1, PixelStatus.block, 12, _Highlight.slash, InteractType.door),
         cost: {stone: 0, wood: 20},
-        label: "Wooden Door - Feels like home"
+        label: "Feels like home"
     },
     {   //stone door
         build: new DoorData("Stone Door", new rgb(200, 200, 200), 1, 1, PixelStatus.block, 24, _Highlight.slash, InteractType.door),
         cost: {stone: 25, wood: 2},
-        label: "Stone Door - a door that will last"
+        label: "A door that will last"
     },
     {   //torch
         build: new LightData("Torch", new rgb(255, 255, 0), 1, 1, 4, 5, 5),
         cost: {stone: 2, wood: 25},
-        label: "Torch - lights up the night, burns out by sunrise"
+        label: "Lights up the night, burns out by sunrise"
     },
 ];
 
 let SelectedBuilding = Building[0];
-document.getElementById("Selected-Building-Label")!.innerHTML = SelectedBuilding.label;
+document.getElementById("Selected-Building-Label")!.innerHTML = SelectedBuilding.build.name + " - " + SelectedBuilding.label;
 document.getElementById("C-Wood")!.innerHTML = '<img src="Icons/wood.png">: ' + SelectedBuilding.cost.wood;
 document.getElementById("C-Stone")!.innerHTML = '<img src="Icons/stone.png">: ' + SelectedBuilding.cost.stone;
 
@@ -104,7 +104,7 @@ function UpdateSelectedBuilding(){
     SelectedBuilding = Building[id];
 
     //update label
-    document.getElementById("Selected-Building-Label")!.innerHTML = SelectedBuilding.label;
+    document.getElementById("Selected-Building-Label")!.innerHTML = SelectedBuilding.build.name + " - " + SelectedBuilding.label;
     //update cost display
     document.getElementById("C-Wood")!.innerHTML = '<img src="Icons/wood.png">: ' + SelectedBuilding.cost.wood;
     document.getElementById("C-Stone")!.innerHTML = '<img src="Icons/stone.png">: ' + SelectedBuilding.cost.stone;
